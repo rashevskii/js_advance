fe48d287d6c63ad96b01264699a960e2669c16af
rashevskii <rashevskii@mail.ru>
2020-11-11 09:40:33 +0400

    Prettier

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000, img: 'img/item.png'},
-    {id: 2, title: 'Mouse', price: 1500, img: 'img/item.png'},
-    {id: 3, title: 'Keyboard', price: 5000, img: 'img/item.png'},
-    {id: 4, title: 'Gamepad', price: 4500, img: 'img/item.png'},
+  { id: 1, title: "Notebook", price: 20000, img: "img/item.png" },
+  { id: 2, title: "Mouse", price: 1500, img: "img/item.png" },
+  { id: 3, title: "Keyboard", price: 5000, img: "img/item.png" },
+  { id: 4, title: "Gamepad", price: 4500, img: "img/item.png" },
 ];
 
-const renderProduct = (title="Title of item", price=0, img) => {
-    return `<div class="product-item">
+const renderProduct = (title = "Title of item", price = 0, img) => {
+  return `<div class="product-item">
                 <img src="${img}">
                 <h3>${title}</h3>
                 <p>${price}</p>
@@ -15,11 +15,13 @@ const renderProduct = (title="Title of item", price=0, img) => {
 };
 
 const catalogInit = (list) => {
-    const productList = list.map((item) => renderProduct(item.title, item.price, item.img));
-    let products = document.querySelector('.products');
-    for(let i = 0; i < productList.length; i++) {
-        products.insertAdjacentHTML('beforeend', productList[i]);
-    }
+  const productList = list.map((item) =>
+    renderProduct(item.title, item.price, item.img)
+  );
+  let products = document.querySelector(".products");
+  for (let i = 0; i < productList.length; i++) {
+    products.insertAdjacentHTML("beforeend", productList[i]);
+  }
 };
 
 catalogInit(products);
